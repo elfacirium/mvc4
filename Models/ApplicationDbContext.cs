51537336e7434a342f70c512a3c7835b4d4ae2a1
elfacirium <122610683+elfacirium@users.noreply.github.com>
2025-10-22 22:24:56 +0200

    Add files via upload

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cvicenie_mvc.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Тут додай таблиці
+        public DbSet<StudentModel> Students { get; set; }
+    }
+}
